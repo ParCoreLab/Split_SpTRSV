272e81743b8c194e29f1afe7e4bc02084fb16089
nahmad16 <nahmad16@ku.edu.tr>
2021-09-07 11:22:05 +0500

    Removed some unused (commented) lines

--- include/SILU_GPU.h
+++ include/SILU_GPU.h
@@ -4,7 +4,4 @@
 // Created: 25-12-2019
 
 #include "SILU.h"
-//#include "ELMR.cuh"
 #include "sptrsv_syncfree_cuda.cuh"
-//#include "SLFC.cuh"
-//int SILU::SyncFreeAnalyzer(device_data_csc *device_csc_matrix, int *inDegree);
